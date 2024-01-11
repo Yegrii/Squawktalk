8b64043314ebea530f7219e6ab8744139dbf10b4
Yegor <viryutinyegor@gmail.com>
2024-01-11 20:01:26 +0200

    4th task

--- practice.sql
+++ practice.sql
@@ -36,3 +36,20 @@ WHERE
 	emp_location = 'UA'
 ORDER BY
 	salary_in_usd;
+
+/*
+Вивести топ 5 з/п серед усіх спеціалістів, які працюють повністю віддалено 
+(remote_ratio = 100)
+*/
+
+SELECT
+	job_title,
+	salary_in_usd
+FROM
+	salaries
+WHERE
+	remote_ratio = 100
+ORDER BY
+	salary_in_usd DESC
+LIMIT 5;
+
