d727dca3aaf7872e650e115c20c226f1f3b26c3d
Yegor <viryutinyegor@gmail.com>
2024-01-11 19:48:34 +0200

    2nd task

--- practice.sql
+++ practice.sql
@@ -7,3 +7,19 @@ FROM
 	salaries
 WHERE
 	job_title = 'ML Engineer';
+	
+/*
+Назвати країну (company_location), в якій зафіксована найменша з/п спеціаліста
+в сфері Data Scientist в 2023 році. 
+*/
+
+SELECT
+	comp_location,
+	salary_in_usd
+FROM
+	salaries
+WHERE
+	job_title = 'Data Scientist'
+ORDER BY
+	salary_in_usd
+LIMIT 1;
