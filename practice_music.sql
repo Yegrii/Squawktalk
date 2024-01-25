57cc8db994a3f73af179aea66977772a8d8c4777
Yegor <viryutinyegor@gmail.com>
2024-01-25 20:53:16 +0200

    add new connection in DBeaver Sample Data

---
+++ practice_music.sql
@@ -0,0 +1,28 @@
+/*
+SELECT 
+	*
+FROM 
+	Invoice i 
+LIMIT 10;
+
+
+SELECT 
+	*
+FROM 
+	InvoiceLine il 
+LIMIT 10;
+
+
+SELECT 
+	*
+FROM 
+	Track t 
+LIMIT 10;
+
+*/
+
+/* 1#
+ Порівняти всіх музичних виконавчів за кількістю проданих музичних треків та загальною сумою продажу
+*/
+
+
